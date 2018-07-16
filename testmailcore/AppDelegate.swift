69d472147a9156a03c2bc2a77d9e2e99f76fafe1
stanchiang <stanchiang23@gmail.com>
2018-07-15 17:37:22 -0700

    upload

---
+++ testmailcore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testmailcore
+//
+//  Created by Stanley Chiang on 7/15/18.
+//  Copyright © 2018 Stanley Chiang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testmailcore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testmailcore
+//
+//  Created by Stanley Chiang on 7/15/18.
+//  Copyright © 2018 Stanley Chiang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
